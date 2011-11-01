64f6f7fb7a856b9e7e74e8fb32588dc0893fa244
Douglas Hall <douglas_hall@harvard.edu>
2011-11-01 14:15:23 -0400

    fixed classmates feed

--- site/Tygra/lib/ClassmatesDataController.php
+++ site/Tygra/lib/ClassmatesDataController.php
@@ -15,14 +15,16 @@
 
          $data = $this->getParsedData();
          $results = array();
-         foreach ($data['classes']['class'][0]['classmate'] as $item) {
-             $person = new PersonObject();
-             $person->setId($item['id']);
-             $person->setFirstName($item['firstname']);
-             $person->setLastName($item['lastname']);
-             $person->setEmail($item['email']);
-             $person->setThumbnail($item['thumbnail']);
-             $results[] = $person->toArray();
+         foreach ($data['classes']['class'] as $class) {
+             foreach ($class['classmate'] as $item) {
+	             $person = new PersonObject();
+	             $person->setId($item['id']);
+	             $person->setFirstName($item['firstname']);
+	             $person->setLastName($item['lastname']);
+	             $person->setEmail($item['email']);
+	             $person->setThumbnail($item['thumbnail']);
+	             $results[] = $person->toArray();
+             }
          }
 
          return $results;
@@ -33,14 +35,16 @@
      	 $this->setBaseUrl('http://vm1.isites.harvard.edu/mobile/classmates.json');
          $data = $this->getParsedData();
          $person = new PersonObject();
-         foreach ($data['classes']['class'][0]['classmate'] as $item) {
-             if ($item['id'] == $id) {
-                 $person->setId($item['id']);
-                 $person->setFirstName($item['firstname']);
-                 $person->setLastName($item['lastname']);
-                 $person->setEmail($item['email']);
-                 $person->setThumbnail($item['thumbnail']);
-                 break;
+         foreach ($data['classes']['class'] as $class) {
+             foreach ($class['classmate'] as $item) {
+	             if ($item['id'] == $id) {
+	                 $person->setId($item['id']);
+	                 $person->setFirstName($item['firstname']);
+	                 $person->setLastName($item['lastname']);
+	                 $person->setEmail($item['email']);
+	                 $person->setThumbnail($item['thumbnail']);
+	                 break;
+	             }
              }
          }
          
