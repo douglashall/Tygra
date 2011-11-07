6254b91b93d29486e285741d05c07630e3531fd6
unknown <old312@.university.harvard.edu>
2011-11-07 15:27:31 -0500

    The initial working version with development authentication module and syllabus (somewhet)

--- site/Tygra/app/modules/syllabus/SyllabusWebModule.php
+++ site/Tygra/app/modules/syllabus/SyllabusWebModule.php
@@ -5,11 +5,14 @@ class SyllabusWebModule extends WebModule
 	protected $id='syllabus';
 	protected function initializeForPage() {
 		$controller = DataController::factory('SyllabusDataController');
+		$session = $this->getSession();
+		$user = $session->getUser();
 	    
 		switch ($this->page)
 		{
 			case 'index':
-				$items = $controller->search('10564158');
+				$items = $controller->search($user);
+				var_export($items);
 				$this->assign('results', $items);
 				break;
 		}
--- site/Tygra/lib/SyllabusDataController.php
+++ site/Tygra/lib/SyllabusDataController.php
@@ -5,25 +5,83 @@
      protected $cacheFolder = "Syllabus"; // set the cache folder
      protected $cacheSuffix = "json";   // set the suffix for cache files
      protected $DEFAULT_PARSER_CLASS='JSONDataParser'; // the default parser
-
-     public function search($q)
+     //user: F7455492-E3B1-11E0-B26E-E0A8BADA4195
+     //pwd: lcnnWEMnqrDLSVL5CifU
+//https://isites.harvard.edu/services/search/select/?userid=70602482&q=Syllabus&omitHeader=true&fq=sitekey:k63478&start=0&rows=250&fl=id,title,description,siteid&wt=json&fq=category:topic
+     public function search($user)
      {
-         // set the base url
-         $this->setBaseUrl('http://vm1.isites.harvard.edu/mobile/syllabus.json');
-         //$this->setBaseUrl('http://localhost:8080/icommonsapi/whatsnew/by_user/10564158.json');
-         //$this->addFilter('alt', 'json'); //set the output format to json
-
-         $data = $this->getParsedData();
-         $results = array();
-         foreach ($data['Courses'] as $item) {
-             $course = new CourseObject();
-             $course->setId($item['courseId']);
-             $course->setTitle($item['title']);
-             $course->setSyllabus($item['syllabusUrl']);
-             $results[] = $course->toArray();
-         }
-
-         return $results;
+     	$syllabusSet = false;
+     	if ($user->getCourses()) {
+     		foreach ($user->getCourses() as $course) {
+     			if($course->getSyllabus()) {
+     				$syllabusSet = true;
+     			}
+     		}
+     	}
+     	if ($syllabusSet ==  false) {
+	     // this an icommonsapi application key
+	     	$SERVER_USER_CREDENTIALS=base64_encode('F7455492-E3B1-11E0-B26E-E0A8BADA4195:lcnnWEMnqrDLSVL5CifU');
+			$sitekeys = '';
+			foreach ($user->getCourses() as $course) {
+				if (strlen($sitekeys) > 0)
+					$sitekeys = $sitekeys.'%20OR%20';
+				$sitekeys = $sitekeys.$course->getKeyword();
+			}
+	     	
+			// set application key for icommonsapi service
+	         $this->addHeader('Authorization', 'Basic ' . $SERVER_USER_CREDENTIALS);
+	         // set the base url
+	         $queryString = sprintf("userid=%s&q=Syllabus&omitHeader=true&fq=category:topic&start=0&rows=100&wt=json&fq=sitekey:%s&fl=topicid,title,sitekey,linkurl,description,fileurl",$user->getUserID(),$sitekeys);
+	         $this->setBaseUrl('https://isites.harvard.edu/services/search/select/'.$queryString);
+	// OTD: I commented out addFilter b/c addFilter does not allow to add multiple params with the same name i.e 'fq'         
+	//         $this->addFilter('userid',$userId);
+	//         $this->addFilter('q','Syllabus');
+	//         $this->addFilter('omitHeader','true');
+	//         $this->addFilter('fq','category:topic');
+	//         $this->addFilter('start','0');
+	//         $this->addFilter('rows','100');
+	//         $this->addFilter('wt','json');
+	//         $this->addFilter('fq','sitekey:k63478');
+	//         $this->addFilter('fl','topicid,title,sitekey,linkurl,description,fileurl');
+	
+	         $data = $this->getParsedData();
+//	         $results = array();
+			foreach ($data['response']['docs'] as $item) {
+				$result = new SyllabusObject();
+				if (isset($item['title']))
+					$result->setTitle($item['title']);
+				if (isset($item['sitekey']))
+					$result->setKeyword($item['sitekey']);
+				if (isset($item['fileurl']))
+					$result->setFileUrl($item['fileurl']);
+				if (isset($item['linkurl']))
+					$result->setLinkUrl($item['linkurl']);
+				if (isset($item['description']))
+				    $result->setDescription($item['description']);
+				if (isset($item['topicid']))
+				    $result->setTopicId($item['topicid']);
+				foreach ($user->getCourses() as $course) {
+					$course->addSyllabusObject($result);
+				}
+	         }
+     	}
+     	
+		$results = array();
+ 		foreach ($user->getCourses() as $course) {
+ 			$view = new SyllabusView();
+ 			$view->setSiteTitle($course->getTitle());
+ 			if($course->getSyllabus()) {
+ 				$items = array();
+				foreach ($course->getSyllabus() as $item) {
+					$items[] = $item->toArray();
+				}
+ 				
+ 				$view->setSyllabus($items);
+ 			}
+			$results[] = $view->toArray();
+ 		}
+     		
+		return $results;
      }
 
      // not used yet
