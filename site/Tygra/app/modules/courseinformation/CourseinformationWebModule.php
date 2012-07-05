db83d867cc9148021ecf571f2b6db9ce59d85ac0
Sapna Mysore <sapna_mysore@harvard.edu>
2012-07-05 15:51:45 -0400

    Added module with correct mixed case in the name

---
+++ site/Tygra/app/modules/courseinformation/CourseinformationWebModule.php
@@ -0,0 +1,28 @@
+<?php
+ 
+ class CourseinformationWebModule extends WebModule
+ {
+     protected $id='courseinformation';
+     protected function initializeForPage() {
+         $session = $this->getSession();
+         $user = $session->getUser();
+         $controller = DataController::factory('CourseInformationDataController');
+ 
+         switch ($this->page)
+         {
+             case 'index':
+                 $keyword = $this->getArg('keyword');
+                 $information = $controller->search($keyword);
+                 
+                 $this->assign('results', $information);
+                 $this->setPageTitles('Course Information');
+                 break;
+         }
+     
+         
+     }
+     
+     
+ }
+ 
+ ?>
